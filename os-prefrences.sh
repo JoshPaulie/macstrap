f91fdaf5fb462f15598fea2317f334e8334d4bba
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-07-15 18:38:31 -0500

    add: more defaults

--- os-prefrences.sh
+++ os-prefrences.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 # Many, if not all, are from the very helpful https://macos-defaults.com/
-# The rest are discovered by diff'ing the defaults file as it was when I created the system, and whatever I decide to compare
+# The rest are discovered by creating a snapshot of `defaults read`, imperatively making the change, snap shotting `defaults read` again, then diff'ing to see what setting was impacted
+
+# Globals
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true" # Show file extension whereever possible
+defaults write NSGlobalDomain "AppleInterfaceStyle" -string "Dark" # Appearance: dark mode
 
 # Finder
 defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
 defaults write com.apple.finder "ShowPathbar" -bool "true"
-defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv" # View style to "List"
 killall Finder
 
 # Dock
 defaults write com.apple.dock "mineffect" -string "scale"
 defaults write com.apple.dock "autohide" -bool "true"
-defaults write com.apple.dock "tilesize" -int "36"
+defaults write com.apple.dock "tilesize" -int "54"
+defaults write com.apple.dock "show-recents" -bool "false"
 killall dock
 
 # Mouse (requires restart)
-defaults write NSGlobalDomain com.apple.mouse.linear -bool "true"
+defaults write NSGlobalDomain com.apple.mouse.linear -bool "true" # Mouse acceleration: disabled
+
+# Keyboard
+# The following 2 greatly speedup the rate of which keys repeat (when holding down on keyboard)
+defaults write NSGlobalDomain "KeyRepeat" -int "2"
+defaults write NSGlobalDomain "InitialKeyRepeat" -int "25"
+
+# Accessibility
+defaults write com.apple.universalaccess "reduceMotion" -bool "true"
+
